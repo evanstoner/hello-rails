152fb9407fac6b8c635a90fa9970e833505aa4c7
Evan Stoner <e.l.stoner@gmail.com>
2014-03-23 19:41:17 -0400

    Add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 HelloRails::Application.routes.draw do
-  resources :posts
-
-  resources :users
+  get "static_pages/home"
+  get "static_pages/help"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
